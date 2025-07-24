201f723b38f52a2942268ad9eec32d98ac76cc3d
Shweta Shinde <shwetashinde2611@gmail.com>
2025-07-24 22:50:24 +0530

    Add files via upload

---
+++ C programming/Assignment 0/06) Area of Triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+ {
+	int Base = 20, Height = 10, Area ;
+	Area = 0.5 * Base * Height;
+
+	printf("Base is = %d and Height is = %d Area of triangle is :: %d ",Base,Height,Area);
+
+}
