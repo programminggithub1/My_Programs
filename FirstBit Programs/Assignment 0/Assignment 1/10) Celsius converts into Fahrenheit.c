2178207bb2331751a71aef79808a3d44d86fe034
Shweta Shinde <shwetashinde2611@gmail.com>
2025-07-24 22:16:48 +0530

    Add files via upload

---
+++ FirstBit Programs/Assignment 0/Assignment 1/10) Celsius converts into Fahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+ {
+	float Celsius = 26, Fahrenheit;
+	Fahrenheit = (Celsius * 9/5) + 32;
+
+	printf("%f Celsius into covert Fahrenheit is :: %f ",Celsius,Fahrenheit);
+
+}
