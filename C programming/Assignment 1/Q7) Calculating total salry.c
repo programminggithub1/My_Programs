c62cd6a94c1a9c331fb1e73d9fe2cac8f8c58e9b
Shweta Shinde <shwetashinde2611@gmail.com>
2025-07-29 22:06:14 +0530

    Add files via upload

---
+++ C programming/Assignment 1/Q7) Calculating total salry.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() {
+    float basic_salary = 8000, da, ta, hra, total_salary;
+    
+    if (basic_salary <= 5000) {
+        da = 0.10 * basic_salary;
+        ta = 0.20 * basic_salary;
+        hra = 0.25 * basic_salary;
+    } else {
+        da = 0.15 * basic_salary;
+        ta = 0.25 * basic_salary;
+        hra = 0.30 * basic_salary;
+    }
+
+    total_salary = basic_salary + da + ta + hra;
+    printf("Total Salary: %f \n", total_salary);
+
+    
+}
